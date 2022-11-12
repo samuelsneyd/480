af781b08449edf3152805e73165ae6bfd7897cf1
Samuel <samuelsneyd@gmail.com>
2022-11-12 15:28:10 +1300

    Add accommodation images

--- src/pages/AccommodationPage/AccommodationPage.tsx
+++ src/pages/AccommodationPage/AccommodationPage.tsx
@@ -5,13 +5,73 @@ import AccommodationIcons from '../../components/AccommodationIcons/Accommodatio
 import CarouselImage from '../../components/CarouselImage/CarouselImage';
 import PageTitle from '../../components/PageTitle/PageTitle';
 import withAnimation from '../../hooks/withAnimation';
-import accommodationImage from '../../assets/images/accommodation.jpg';
+import image1 from '../../assets/images/bedroom.webp';
+import image2 from '../../assets/images/loft.webp';
+import image3 from '../../assets/images/bathroom.webp';
+import image4 from '../../assets/images/kitchen.webp';
+import image5 from '../../assets/images/dining.webp';
+import image6 from '../../assets/images/shower.webp';
+import image7 from '../../assets/images/deck.webp';
+import image8 from '../../assets/images/deck_b.webp';
+import image9 from '../../assets/images/trail.webp';
+import image10 from '../../assets/images/meadow.webp';
+import image11 from '../../assets/images/meadow2.webp';
 
 const images = [
   {
-    title: 'Accommodation',
-    alt: 'Accommodation',
-    src: accommodationImage,
+    title: 'Bedroom',
+    alt: 'Bedroom',
+    src: image1
+  },
+  {
+    title: 'Loft',
+    alt: 'Loft',
+    src: image2
+  },
+  {
+    title: 'Bathroom',
+    alt: 'Bathroom',
+    src: image3
+  },
+  {
+    title: 'Kitchen',
+    alt: 'Kitchen',
+    src: image4
+  },
+  {
+    title: 'Dining',
+    alt: 'Dining',
+    src: image5
+  },
+  {
+    title: 'Shower',
+    alt: 'Shower',
+    src: image6
+  },
+  {
+    title: 'Deck A',
+    alt: 'Deck A',
+    src: image7
+  },
+  {
+    title: 'Deck B',
+    alt: 'Deck B',
+    src: image8
+  },
+  {
+    title: 'Trail',
+    alt: 'Trail',
+    src: image9
+  },
+  {
+    title: 'Meadow A',
+    alt: 'Meadow A',
+    src: image10
+  },
+  {
+    title: 'Meadow B',
+    alt: 'Meadow B',
+    src: image11
   }
 ];
 
