2e0453617b5102cb7dfcf3fc9c6981eed82780e4
Samuel <samuelsneyd@gmail.com>
2022-11-12 13:27:32 +1300

    Add deployment workflow

--- deploy.sh
+++ deploy.sh
@@ -20,10 +20,10 @@ git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# if you are deploying to https://<USERNAME>..github.io
+# git push -f git@.github.com:<USERNAME>/<USERNAME>..github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# if you are deploying to https://<USERNAME>..github.io/<REPO>
 git push -f git@github.com:samuelsneyd/480.git main:gh-pages
 
 cd -
