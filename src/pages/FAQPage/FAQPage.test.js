38b8b9a35ecea12253613eb4695935b38cd6fb86
Samuel <samuelsneyd@gmail.com>
2022-11-14 14:34:50 +1300

    Init jest testing library config

--- src/pages/FAQPage/FAQPage.test.js
+++ src/pages/FAQPage/FAQPage.test.js
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import FAQPage from './FAQPage';
 
-jest.mock('../../../static/faq/faq.md', () => () => '<div>FAQ</div>');
-
 it('renders FAQPage without crashing', () => {
   const div = document.createElement('div');
   const root = createRoot(div);
